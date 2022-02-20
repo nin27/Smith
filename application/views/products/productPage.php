108d1811b095d388657e125635a92c4fd94824fc
nin27 <62254288+nin27@users.noreply.github.com>
2022-02-20 18:41:47 +0800

    init

--- application/views/products/productPage.php
+++ application/views/products/productPage.php
@@ -1,14 +1,15 @@
-<h2><?=$page?></h2>
+<h2><?= $page ?></h2>
+
 
 
 <div class="row">
     <div class="container" style="width:800px;">
-      <?php foreach($products as $product):?>
-      <div class="col-md-5">
-        <div class="thumbnail"> <img src="<?php echo base_url("assets/{$product['image']}")?>" alt="Lights" class="img-responsive" style="width:100%; height:200px;">
+        <?php foreach ($products as $product) : ?>
+            <!-- <div class="col-md-5">
+        <div class="thumbnail"> <img src="<?php echo base_url("assets/{$product['image']}") ?>" alt="Lights" class="img-responsive" style="width:100%; height:200px;">
           <div class="caption">
-            <p style="text-align:center;"><?php print $product['name']?></p>
-            <p style="text-align:center;color:#04B745;"><b>$<?php print $product['price']?></b></p><br>
+            <p style="text-align:center;"><?php print $product['name'] ?></p>
+            <p style="text-align:center;color:#04B745;"><b>$<?php print $product['price'] ?></b></p><br>
 			<button type="submit" class="btn btn-warning" style="margin:0;">Preview</button>
             <form method="post" action="moro.php?action=addcart">
               <p style="text-align:center;color:#04B745;">
@@ -18,13 +19,22 @@
 					alert("Item Added to Cart")
 				} 
 				</script>
-                <input type="hidden" name="sku" value="<?php print $product['sku']?>">
+                <input type="hidden" name="sku" value="<?php print $product['sku'] ?>">
               </p>
             </form>
           </div>
         </div>
-      </div>
-      <?php endforeach;?>
-    </div>
-  </div>
+      </div> -->
 
+            <div class="card" style="width: 18rem;">
+                <img src="<?php echo base_url("assets/{$product['image']}") ?>" class="card-img-top" alt="...">
+                <div class="card-body">
+                    <h5 class="card-title"><?php print $product['name'] ?></h5>
+                    <p class="card-text text-center">$<?php print $product['price'] ?></p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+
+        <?php endforeach; ?>
+    </div>
+</div>
