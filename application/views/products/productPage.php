97aa17282505311dbe6eac3af5e0ff945d64fb60
nin27 <62254288+nin27@users.noreply.github.com>
2022-02-20 19:29:36 +0800

    Update productPage.php

--- application/views/products/productPage.php
+++ application/views/products/productPage.php
@@ -3,21 +3,19 @@
 
 <div class="col-md-12">
     <div class="row">
-    
     <?php foreach ($products as $product) : ?>
-            
                 <div class="col-sm-4">
                     <div class="card" style="width: 18rem;">
-                        <img src="<?php echo base_url("assets/{$product['image']}") ?>" class="card-img-top" alt="...">
-                        <div class="card-body">
-                            <h5 class="card-title"><?php print $product['name'] ?></h5>
-                            <p class="card-text text-center">$<?php print $product['price'] ?></p>
+                        <img src="<?php echo base_url("assets/{$product['image']}") ?>" class="card-img-top" alt="<?=$product['name'] ?>" style="width:100%; height:170px;">
+                        <div class="card-body text-center">
+                            <h5 class="card-title"><?=$product['name'] ?></h5>
+                            <p class="card-text ">$<?=$product['price'] ?></p>
                             <a href="#" class="btn btn-primary">Preview</a>
+                            <a href="#" class="btn btn-primary">Add to cart</a>
                         </div>
                     </div>
                 </div>
     
-        <?php 
-        endforeach; ?>
+        <?php endforeach; ?>
     </div>
 </div>
