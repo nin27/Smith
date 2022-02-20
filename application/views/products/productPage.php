eeb6fbbcc90c0420004502fdd49aff7c824da3cc
nin27 <62254288+nin27@users.noreply.github.com>
2022-02-20 19:18:51 +0800

    Update productPage.php

--- application/views/products/productPage.php
+++ application/views/products/productPage.php
@@ -1,40 +1,23 @@
 <h2><?= $page ?></h2>
 
 
-
-<div class="row">
-    <div class="container" style="width:800px;">
-        <?php foreach ($products as $product) : ?>
-            <!-- <div class="col-md-5">
-        <div class="thumbnail"> <img src="<?php echo base_url("assets/{$product['image']}") ?>" alt="Lights" class="img-responsive" style="width:100%; height:200px;">
-          <div class="caption">
-            <p style="text-align:center;"><?php print $product['name'] ?></p>
-            <p style="text-align:center;color:#04B745;"><b>$<?php print $product['price'] ?></b></p><br>
-			<button type="submit" class="btn btn-warning" style="margin:0;">Preview</button>
-            <form method="post" action="moro.php?action=addcart">
-              <p style="text-align:center;color:#04B745;">
-                <button onclick="addbtn()" type="submit" class="btn btn-warning">Add To Cart</button>
-				<script>
-				function addbtn(){
-					alert("Item Added to Cart")
-				} 
-				</script>
-                <input type="hidden" name="sku" value="<?php print $product['sku'] ?>">
-              </p>
-            </form>
-          </div>
-        </div>
-      </div> -->
-
-            <div class="card" style="width: 18rem;">
-                <img src="<?php echo base_url("assets/{$product['image']}") ?>" class="card-img-top" alt="...">
-                <div class="card-body">
-                    <h5 class="card-title"><?php print $product['name'] ?></h5>
-                    <p class="card-text text-center">$<?php print $product['price'] ?></p>
-                    <a href="#" class="btn btn-primary">Go somewhere</a>
+<div class="col-md-12">
+    <div class="row">
+    
+    <?php foreach ($products as $product) : ?>
+            
+                <div class="col-sm-4">
+                    <div class="card" style="width: 18rem;">
+                        <img src="<?php echo base_url("assets/{$product['image']}") ?>" class="card-img-top" alt="...">
+                        <div class="card-body">
+                            <h5 class="card-title"><?php print $product['name'] ?></h5>
+                            <p class="card-text text-center">$<?php print $product['price'] ?></p>
+                            <a href="#" class="btn btn-primary">Preview</a>
+                        </div>
+                    </div>
                 </div>
-            </div>
-
-        <?php endforeach; ?>
+    
+        <?php 
+        endforeach; ?>
     </div>
 </div>
